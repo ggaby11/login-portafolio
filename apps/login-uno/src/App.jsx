bcf0f27075404db2f28917710900525dd05b4bb3
ggaby11 <ggabysuarez.gomez@gmail.com>
2025-04-20 00:11:47 -0500

    Add CSS to Login

--- apps/login-uno/src/App.jsx
+++ apps/login-uno/src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react'
-import './index.css'
+import './app.css';
+import { TiSocialFacebook, TiSocialGooglePlus, TiSocialLinkedin } from "react-icons/ti";
 
 function App() {
   return (
-      <div id='fondo' className='h-screen w-screen bg-blue-100'>
-        <div className='h-1/4'></div>
-        <div id='contenedor' className='flex text-center items-center ml-20 mr-20 h-100 justify-center border border-1g border-gray-300 shadow-lg bg-red-100'>
-          <div id='ingreso' className='flex flex-col w-1/2 bg-pink-100'>
-            <p className='text-2xl'>Ingresa Sesión con</p>
+      <div id='fondo' className='h-screen w-screen bg-gray-100'>
+        <div className='h-1/5'></div>
+        <div id='contenedor' className='flex text-center items-center justify-center ml-20 mr-20 h-100 md:ml-80 md:mr-80 lg:ml-150 lg:mr-150 border rounded-xl shadow-2xl bg-white'>
+          <div className='flex flex-col items-center justify-center w-1/2 h-full mr-10 ml-10'>
+            <p className='text-2xl font-bold'>Ingresa Sesión con</p>
+
+            <div className='flex gap-3 mt-2 mb-10'>
+              <button id='social-media' className='bg-blue-800'><TiSocialFacebook /></button>
+              <button id='social-media' className='bg-red-700'><TiSocialGooglePlus  /></button>
+              <button id='social-media' className='bg-blue-500'><TiSocialLinkedin /></button>
+            </div>
+
             <p>o ingresa usando tu cuenta</p>
             <input type='email' placeholder='Email'/>
             <input type='password' placeholder='Contraseña'/>
-            <button>¿Olvidaste tu contraseña?</button>
-            <button>Ingresar</button>
+            <button id='btnRecuperar' className='mt-3 text-gray-700'>¿Olvidaste tu contraseña?</button>
+            <button id='btnIngresar' className='mt-3'>Ingresar</button>
           </div>
-          <div id='registro' className='flex flex-col items-center w-1/2 bg-white'>
-            <p className='text-2xl'>¿Aún no estas registrado?</p>
-            <p className='text-xl'>¡ingresa tus datos e inicia el viaje con nosotros!</p>
-            <button>Registrarse</button>
+          <div className='flex flex-col items-center justify-center w-1/2 h-full mr-10 ml-10 gap-4'>
+            <p className='text-2xl font-bold'>¿Aún no estas registrado?</p>
+            <p className='text-xl'>¡Ingresa tus datos e inicia el viaje con nosotros!</p>
+            <button id='btnRegistrar'>Registrarse</button>
           </div>
         </div>
       </div>
---
+++ apps/login-uno/src/script.js
@@ -0,0 +1,7 @@
+document.getElementById('ingresar').addEventListener("click", function () {
+    document.getElementById('contenedor').classList.add("activar.panel.derecho");
+});
+
+document.getElementeById("registrar").addEventListener("click", function () {
+    document.getElementById('contenedor').classList.add("activar.panel.derecho");
+});
